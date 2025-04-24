d8848239963e6565fe1ebcdbd98b1a2af4e18912
Oscar <oscar.marban@universidadeuropea.es>
2025-04-24 12:56:33 +0200

    Mensaje de prueba desde terminal. #1 commit

--- PruebaGIT/src/src/Prueba.java
+++ PruebaGIT/src/src/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 
 	public Prueba() {
 		// TODO Auto-generated constructor stub
-		System.out.println("Hola mundo");
+		System.out.println("Cambiando el texto del hola mundo");
 	}
 	
 	public static void f1(int a) {
