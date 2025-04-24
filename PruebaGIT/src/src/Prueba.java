ad142551bbad266b3c2818e962f4ffa43f2716d5
Oscar <oscar.marban@universidadeuropea.es>
2025-04-24 15:01:53 +0200

    Prueba

--- PruebaGIT/src/src/Prueba.java
+++ PruebaGIT/src/src/Prueba.java
@@ -4,32 +4,11 @@ public class Prueba {
 
 	public Prueba() {
 		// TODO Auto-generated constructor stub
-		System.out.println("Cambiando el texto del hola mundo");
+		System.out.println("Hola mundo");
 	}
 	
 	public static void f1(int a) {
 		System.out.println("f1");
 	}
-	
-	public static void f3(int a) {
-		System.out.println("f3");
-		System.out.println("Esto es una prueba");
-	}
-	
-	public static void f2() {
-		System.out.println("f2");
-		System.out.println("f2++");
-	}
 
-	public static void f6() {
-		System.out.println("f6");
-	}
-
-	public static void f5() {
-		System.out.println("f5");	
-	}
-
-	public static void f7() {
-		System.out.println("f7");	
-	}
 }
