d7ad2252b4384fd7698bf77fd4fc7f5284adfc14
Oscar <oscar.marban@universidadeuropea.es>
2025-04-24 15:33:53 +0200

    ddd

---
+++ PruebaGIT/src/src/Prueba.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Prueba {
+
+	public Prueba() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Hola mundo");
+	}
+	
+	public static void f1(int a) {
+		System.out.println("f1");
+	}
+
+}
