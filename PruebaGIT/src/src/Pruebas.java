ff68307a6658ad10d2daa1d6ec45406f5abccfb3
Oscar <oscar.marban@universidadeuropea.es>
2025-04-24 15:11:51 +0200

    ambio nombre fichero

---
+++ PruebaGIT/src/src/Pruebas.java
@@ -0,0 +1,37 @@
+public class Pruebas {
+
+	public Pruebas() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Cambiando el texto del hola mundo");
+	}
+	
+	public static void f1(int a) {
+		System.out.println("f1");
+	}
+	
+	public static void f3(int a) {
+		System.out.println("f3");
+		System.out.println("Esto es una prueba");
+	}
+	
+	public static void f2() {
+		System.out.println("f2");
+		System.out.println("f2++");
+	}
+
+	public static void f6() {
+		System.out.println("f6");
+	}
+
+	public static void f5() {
+		System.out.println("f5");	
+	}
+
+	public static void f7() {
+		System.out.println("f7");	
+	}
+
+	public static void f8() {
+		System.out.println("f8");	
+	}
+}
